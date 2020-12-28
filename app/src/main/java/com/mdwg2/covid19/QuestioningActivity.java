e9ea3d1b1e5f3ce8fa1c4b558ca39366db096cf8
RouaSouissi <rouasouissi98@gmail.com>
2020-12-28 16:12:32 +0100

    roua_suissi interface3

---
+++ app/src/main/java/com/mdwg2/covid19/QuestioningActivity.java
@@ -0,0 +1,25 @@
+package com.mdwg2.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.awt.Button;
+
+import javax.swing.text.View;
+
+public class QuestioningActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questioning);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (v.getId() == R.id.button) {
+                    Toast.makeText(getApplicationContext(), "you clicked the Next button", Toast.LENGTH_SHORT).show();
+                    Intent sondage = new Intent(this, activity_sondage_main.Class);
+                    startActivity(sondage);
+
+                }}
+        });
+        }}
