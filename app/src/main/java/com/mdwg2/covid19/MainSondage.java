ef1bd13c34ab3ec9e64c3b41db8c530eef06dddf
chaimakhemiri <chimoutykh@gmail.com>
2020-12-29 16:03:48 +0100

    Initial commit

--- app/src/main/java/com/mdwg2/covid19/MainSondage.java
+++ app/src/main/java/com/mdwg2/covid19/MainSondage.java
@@ -1,4 +1,32 @@
 package com.mdwg2.covid19;
 
-public class MainSondage {
+public class MainSondage {package com.mdwg2.covid19;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+    public class MainSondage<button> extends AppCompatActivity implements View.OnClickListener   {
+        Button button;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            button = findViewById(R.id.bouton);
+            button.setOnClickListener(this);
+
+
+        }
+
+        public void onClick(View v) {
+            if (v.getId() == R.id.bouton) {
+                Toast.makeText(getApplicationContext(), "start now", Toast.LENGTH_SHORT).show();
+                Intent Survey = new Intent(this, act_connexion1.Class);
+                startActivity(Survey);
+            }
+        }
+    }
 }
