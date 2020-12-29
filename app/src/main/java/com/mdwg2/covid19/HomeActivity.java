6c526e64d437711806d711507803416242558957
chaimakhemiri <chimoutykh@gmail.com>
2020-12-29 16:54:39 +0100

    Initial commit

--- app/src/main/java/com/mdwg2/covid19/HomeActivity.java
+++ app/src/main/java/com/mdwg2/covid19/HomeActivity.java
@@ -4,7 +4,7 @@ public class MainActivity {package com.mdwg2.covid19;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Button1;
 import android.widget.Toast;
 
    public class MainActivity extends AppCompatActivity implements View.OnClickListener   {
@@ -13,14 +13,14 @@ import android.widget.Toast;
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            button1 = findViewById(R.id.bouton);
+            button1 = findViewById(R.id.bouton1);
             button1.setOnClickListener(this);
 
 
         }
 
         public void onClick(View v) {
-            if (v.getId() == R.id.bouton) {
+            if (v.getId() == R.id.bouton1) {
                 Toast.makeText(getApplicationContext(), "start now", Toast.LENGTH_SHORT).show();
                 Intent Start = new Intent(this, act_connexion1.Class);
                 startActivity(Start);
