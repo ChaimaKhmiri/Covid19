1a762a89eb7ff832079d9b841a9273ae1d7eb0c8
RouaSouissi <rouasouissi98@gmail.com>
2020-12-30 19:24:57 +0100

    roua_suissi interface3

--- app/src/main/java/com/mdwg2/covid19/HomeActivity.java
+++ app/src/main/java/com/mdwg2/covid19/HomeActivity.java
@@ -2,19 +2,24 @@ package com.mdwg2.covid19;
 
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import javax.swing.text.html.ImageView;
 
-import android.view.View;
-import android.widget.Button;
-
-import android.widget.TextView;
-import android.widget.Toast;
-
-   public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
        Button button1;
+       public void onClickImage(View view){
+           ImageView ImageView = (javax.swing.text.html.ImageView) findViewById(R.id.virus);
+           ImageView.animate().alpha(0.4f).setDuration(2000);
+           //ImageView.animate().translationX(1000).setDuration(2000);
+           ImageView.animate().scaleX(5).setDuration(2000);
+           // ImageView.animate().rotation(360).setDuration(2000);
+       }
+
 
        @Override
        protected void onCreate(Bundle savedInstanceState) {
